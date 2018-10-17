8ec9246b2b71bfec11f270335b92d9baff835b44
Joel Margolese <joel@truerowing.com>
2018-10-17 16:15:28 -0400

    Add seekToEnd for iOS live

--- ios/BrightcovePlayerManager.m
+++ ios/BrightcovePlayerManager.m
@@ -48,5 +48,14 @@ RCT_EXPORT_METHOD(seekTo:(nonnull NSNumber *)reactTag seconds:(nonnull NSNumber 
     }];
 }
 
+RCT_EXPORT_METHOD(seekToEnd:(nonnull NSNumber *)reactTag) {
+    [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+        BrightcovePlayer *player = (BrightcovePlayer*)viewRegistry[reactTag];
+        if ([player isKindOfClass:[BrightcovePlayer class]]) {
+            [player seekToEnd];
+        }
+    }];
+}
+
 
 @end
