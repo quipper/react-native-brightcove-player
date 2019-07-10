89eb677544342dd2ba1701665afe5e9a2308c377
Rafael Cardoso <rcardoso@brightcove.com>
2019-07-10 11:24:12 +1000

    Added a Live example (without DVR)

--- example/App.js
+++ example/App.js
@@ -1,6 +1,7 @@
 import Home from './components/Home';
 import BrightcoveHeader from './components/video-header';
 import BrightcoveScroll from './components/video-scroll';
+import BrightcoveLiveNoDVR from './components/video-live-no-dvr';
 import NestedView from './components/nested-view';
 import ArticleScreen from './components/article';
 import VideoScreen from './components/video';
@@ -31,6 +32,7 @@ const MainNavigator = createStackNavigator({
 	Home: { screen: Home },
 	BrightcoveHeader: { screen: BrightcoveHeader },
 	BrightcoveScroll: { screen: BrightcoveScroll },
+	BrightcoveLiveNoDVR: { screen: BrightcoveLiveNoDVR },
 	NestedView: {screen: NestedView},
 	ArticleStack: { screen: ArticleStack },
 
---
+++ example/components/video-live-no-dvr.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header } from 'react-navigation';
+import {BCPlayer} from 'react-native-brightcove-player';
+
+const ACCOUNT_ID = '1872491397001';
+const POLICY_KEY = 'BCpkADawqM2kD-MtMQswS0cLWgf553m4yFUj8vRkvNVw6wybPb1CSVo3Y4mPyR7RQPv5zMoJbxYZpJMBeHhHJYFW4_FIfrvRvid1_xNlUCkCr8mdh35esbt0gJsqi-C_zIXH8xpXRIeiM_44';
+const VIDEO_ID = '6057201331001'; // Live in Australia
+//const VIDEO_ID = '1872491397001'; // Live in USA
+
+
+
+const AppHeader = (headerProps) => <Header {... headerProps} />;
+
+export default class VideoHeader extends Component {
+
+	static navigationOptions = ({ navigation }) => {
+		return {
+			headerTitle: 'Live - NO DVR',
+			header: navigation.state.params ? navigation.state.params.header : AppHeader
+		}
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<BCPlayer
+					style={styles.player}
+					accountId={ACCOUNT_ID}
+					policyKey={POLICY_KEY}
+					videoId={VIDEO_ID}
+					play={true}
+					autoPlay={true}
+					fullscreen={false}
+					onFullScreen={isLandscape => {
+						isLandscape ? this.props.navigation.setParams({
+							header: null
+						}) : this.props.navigation.setParams({
+							header: AppHeader
+						})
+					}}
+					onEvent={(event) => {
+						console.log(event);
+					}}
+					rotateToFullScreen
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+	player: {
+		width: '100%',
+		aspectRatio: 16/9,
+		backgroundColor: '#000000'
+	}
+});
