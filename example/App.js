db4ba289582625466d5510d8fd46ed9bf4d992de
Ryota Mannari <info@manse.jp>
2019-10-21 17:08:37 +0900

    feat: Reimplements example app

--- example/App.js
+++ example/App.js
@@ -1,114 +1,213 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { FlatList, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { BrightcovePlayer, BrightcovePlayerPoster, BrightcovePlayerUtil } from 'react-native-brightcove-player';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const ACCOUNT_ID = '5434391461001';
+const POLICY_KEY =
+  'BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L';
+const PLAYLIST_REF_ID = 'brightcove-native-sdk-plist';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  state = {
+    videos: [],
+    offlineVideos: [],
+    playback: {
+      referenceId: null,
+      videoToken: null
+    }
+  };
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  componentDidMount() {
+    BrightcovePlayerUtil.getPlaylistWithReferenceId(
+      ACCOUNT_ID,
+      POLICY_KEY,
+      PLAYLIST_REF_ID
+    )
+      .then(videos => {
+        this.setState({
+          videos
+        });
+      })
+      .catch(console.warn);
+    BrightcovePlayerUtil.getOfflineVideoStatuses(ACCOUNT_ID, POLICY_KEY)
+      .then(offlineVideos => {
+        this.setState({
+          offlineVideos
+        });
+      })
+      .catch(console.warn);
+    this.disposer = BrightcovePlayerUtil.addOfflineNotificationListener(
+      offlineVideos => {
+        this.setState({
+          offlineVideos
+        });
+      }
+    );
+  }
+
+  requestDownload(videoId) {
+    BrightcovePlayerUtil.requestDownloadVideoWithVideoId(
+      ACCOUNT_ID,
+      POLICY_KEY,
+      videoId
+    ).catch(() => {});
+  }
+
+  play(item) {
+    const downloadStatus = this.state.offlineVideos.find(
+      video => video.videoId === item.videoId
+    );
+    this.setState({
+      playback:
+        downloadStatus && downloadStatus.downloadProgress === 1
+          ? {
+              videoToken: downloadStatus.videoToken
+            }
+          : {
+              referenceId: item.referenceId
+            }
+    });
+  }
+
+  delete(videoToken) {
+    BrightcovePlayerUtil.deleteOfflineVideo(
+      ACCOUNT_ID,
+      POLICY_KEY,
+      videoToken
+    ).catch(console.warn);
+  }
+
+  componentWillUnmount() {
+    this.disposer && this.disposer();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <BrightcovePlayer
+          style={styles.video}
+          accountId={ACCOUNT_ID}
+          policyKey={POLICY_KEY}
+          autoPlay
+          {...this.state.playback}
+        />
+        <FlatList
+          style={styles.list}
+          extraData={this.state.offlineVideos}
+          data={this.state.videos}
+          keyExtractor={item => item.referenceId}
+          renderItem={({ item }) => {
+            const downloadStatus = this.state.offlineVideos.find(
+              video => video.videoId === item.videoId
+            );
+            return (
+              <View style={styles.listItem}>
+                <TouchableOpacity
+                  style={styles.mainButton}
+                  onPress={() => this.play(item)}
+                >
+                  <BrightcovePlayerPoster
+                    style={styles.poster}
+                    accountId={ACCOUNT_ID}
+                    policyKey={POLICY_KEY}
+                    referenceId={item.referenceId}
+                  />
+                  <View style={styles.body}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text>{item.description}</Text>
+                    {downloadStatus ? (
+                      <Text style={styles.offlineBanner}>
+                        {downloadStatus.downloadProgress === 1
+                          ? 'OFFLINE PLAYBACK'
+                          : `DOWNLOADING: ${Math.floor(
+                              downloadStatus.downloadProgress * 100
+                            )}%`}
+                      </Text>
+                    ) : null}
+                    <Text style={styles.duration}>
+                      {`0${Math.floor(item.duration / 60000) % 60}`.substr(-2)}:
+                      {`0${Math.floor(item.duration / 1000) % 60}`.substr(-2)}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.downloadButton}
+                  onPress={() => {
+                    if (!downloadStatus) {
+                      this.requestDownload(item.videoId);
+                    } else {
+                      this.delete(downloadStatus.videoToken);
+                    }
+                  }}
+                >
+                  <Text>
+                    {!downloadStatus
+                      ? '💾'
+                      : downloadStatus.downloadProgress === 1
+                      ? '🗑'
+                      : '⏳'}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    flexDirection: 'column'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  video: {
+    width: '100%',
+    height: 260
+  },
+  list: {
+    flex: 1
+  },
+  listItem: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray'
+  },
+  mainButton: {
+    flex: 1,
+    flexDirection: 'row'
   },
   body: {
-    backgroundColor: Colors.white,
+    flex: 1,
+    padding: 10,
+    flexDirection: 'column'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  name: {
+    fontSize: 14,
+    fontWeight: 'bold'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  offlineBanner: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'flex-start',
+    padding: 3,
+    backgroundColor: 'deepskyblue'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  duration: {
+    marginTop: 'auto',
+    opacity: 0.5
   },
-  highlight: {
-    fontWeight: '700',
+  poster: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black'
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  downloadButton: {
+    padding: 16,
+    marginLeft: 'auto',
+    alignSelf: 'center'
+  }
 });
-
-export default App;
